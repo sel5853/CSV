647d2002b843a277ca26fc716746a1af5d24b619
BT_1N3_24 <BT_1N3_24@BT_1N3_24.bktech.local>
2018-03-19 09:22:52 -0400

    1

---
+++ HelloWorld/src/application/HelloWorld.java
@@ -0,0 +1,25 @@
+package application;
+
+import javafx.application.Application;
+import javafx.event.ActionEvent;
+import javafx.event.EventHandler;
+import javafx.scene.Scene;
+import javafx.scene.control.Button;
+import javafx.scene.layout.StackPane;
+import javafx.stage.Stage;
+
+public class HelloWorld extends Application{
+
+	public static void main(String[] args)
+	{
+		launch(args);
+	}
+	
+	@Override
+	public void start(Stage primaryStage)
+	{
+		primaryStage.setTitle("Hello World!");
+		Button btn = new Button();
+		btn.
+	}
+}
